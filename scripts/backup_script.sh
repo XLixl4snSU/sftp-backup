78f7d9efb18e932fce687de001861decbbcf455e
XLixl4snSU <111450124+XLixl4snSU@users.noreply.github.com>
2022-09-18 16:13:50 +0200

    Update backup_script.sh

--- scripts/backup_script.sh
+++ scripts/backup_script.sh
@@ -4,11 +4,11 @@
 #Test2
 if [ -z $backup_bwlimit ]
 then
-backup_bwlimit=4M
+  backup_bwlimit=4M
 fi
 datum=$(date +%F)
 sshfs -p $backup_port -o BatchMode=yes,IdentityFile=/home/ssh/ssh_host_rsa_key,StrictHostKeyChecking=accept-new,_netdev,reconnect $backup_nutzername@$backup_adresse:/ /mnt/sftp/
 rclone sync --create-empty-src-dirs -v /mnt/sftp/backup/ /mnt/lokal/ --bwlimit $backup_bwlimit --log-file /config/logs/duplicati-sftp-$datum.log --stats 30s
 cp /config/logs/duplicati-sftp-$datum.log /mnt/sftp/statistik/duplicati-sftp-$datum.log
 umount -lf /mnt/sftp/
-
+echo Backup beendet.
