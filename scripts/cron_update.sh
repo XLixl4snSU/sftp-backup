7b8a76ec8b2173a0ba63660a6c3da1dea4566e84
XLixl4snSU <111450124+XLixl4snSU@users.noreply.github.com>
2022-09-23 16:27:17 +0200

    Implementierung von File-Locks
    - Lock des Cronjobs (keine mehrfache Ausführung)
    - Backup Script prüft auf file.lock im backup/-Verzeichnis vor Beginn des Backups
    - Mehr Informationen im Script für Log
    - Anpassung Standard-Backup Zeit auf 03:15 Uhr

--- scripts/cron_update.sh
+++ scripts/cron_update.sh
@@ -2,23 +2,21 @@
 # DO NOT MODIFY THIS LINE 5M7AnOtp5R8XZlcgkQrSntFgW6gXgm7M
 
 # Editable Variables:
-backup_cron_freq="15 5 * * *"
+backup_cron_freq="10 3 * * *"
 setup_cron_freq="5 0 * * *"
 
 if [ -z "$backup_manuelle_frequenz" ]
 then
-  echo Keine manuelle Cron-Frequenz gesetzt, verwende Standardfrequenz: $backup_cron_freq
+  echo "Keine manuelle Cron-Frequenz gesetzt, verwende Standardfrequenz: $backup_cron_freq"
 else
   backup_cron_freq=$backup_manuelle_Frequenz
-  echo Manuelle Cron-Frequenz gesetzt, diese wird verwendet und lautet $backup_cron_freq
+  echo "Manuelle Cron-Frequenz gesetzt, diese wird verwendet und lautet $backup_cron_freq"
 fi
 
 cronedit () {
   crontab -l > cron.temp
   sed -i '/\/home\/scripts\/backup_script.sh/d' cron.temp
-  echo "$backup_cron_freq /home/scripts/backup_script.sh">>cron.temp
-  sed -i '/\/home\/scripts\/setup.sh/d' cron.temp
-  echo "$setup_cron_freq /home/scripts/setup.sh">>cron.temp
+  echo "$backup_cron_freq flock -n /tmp/backup.lock /home/scripts/backup_script.sh">>cron.temp
   crontab cron.temp
   rm -f cron.temp
   echo Crontab aktualisiert. Starte crond.
