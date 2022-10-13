9c1f0ab3a117d5fcebb2733087b84d75f91a9c1f
XLixl4snSU <111450124+XLixl4snSU@users.noreply.github.com>
2022-10-13 21:39:31 +0200

    Docker Logs, kleine Fixes

--- scripts/backup_now.sh
+++ scripts/backup_now.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flock -n /tmp/backup.lock /home/scripts/backup_script.sh |& tee /config/logs/backup_script-$(date +%F).log
+flock -n /tmp/backup.lock /home/scripts/backup_script.sh |& tee /config/logs/backup_script-$(date +%F).log /var/log/container.log
